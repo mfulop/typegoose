521048ebac8c39daea4a8937ec09eea96ae0ce20
matt <mattjennings44@gmail.com>
2018-05-14 00:28:26 -0500

    separate big typegoose test into smaller tests

--- __tests__/typegoose/typegoose/typegoose.spec.ts
+++ __tests__/typegoose/typegoose/typegoose.spec.ts
@@ -14,10 +14,40 @@ import { InstanceType } from 'src/typegoose';
 import 'jest-extended';
 
 describe('Typegoose', () => {
-  beforeEach(() => initDatabase());
-  afterEach(() => closeDatabase());
+  beforeEach(async () => {
+    await initDatabase();
+  });
+  afterEach(async () => {
+    await closeDatabase();
+  });
 
-  it('should create a User with connections', async () => {
+  it('should work with findOrCreate', async () => {
+      const createdUser = await User.findOrCreate({
+        firstName: 'Jane',
+        lastName: 'Doe',
+        gender: Genders.FEMALE,
+      });
+
+      expect(createdUser).toBeTruthy();
+      expect(createdUser).toHaveProperty('created');
+      expect(createdUser.created).toEqual(true);
+      expect(createdUser).toHaveProperty('doc');
+      expect(createdUser.doc.firstName).toEqual('Jane');
+
+      const foundUser = await User.findOrCreate({
+        firstName: 'Jane',
+        lastName: 'Doe',
+      });
+
+      expect(foundUser).toBeTruthy();
+      expect(foundUser).toHaveProperty('created');
+      expect(foundUser.created).toEqual(false);
+      expect(foundUser).toHaveProperty('doc');
+      expect(foundUser.doc.firstName).toEqual('Jane');
+
+  })
+
+  it ('should create a user with references to cars', async () => {
     const car = await Car.create({
       model: 'Tesla',
       price: Decimal128.fromString('50123.25'),
@@ -57,101 +87,98 @@ describe('Typegoose', () => {
       previousCars: [trabant.id, zastava.id],
     });
 
-    {
-      const foundUser = await User
+    const foundUser = await User
         .findById(user.id)
         .populate('car previousCars')
         .exec();
 
-      expect(foundUser.nick).toEqual('Nothing');
-      expect(foundUser.firstName).toEqual( 'John');
-      expect(foundUser.lastName).toEqual('Doe');
-      expect(foundUser.uniqueId).toEqual('john-doe-20');
-      expect(foundUser.age).toEqual( 20);
-      expect(foundUser.gender).toEqual(Genders.MALE);
-      expect(foundUser.role).toEqual(Role.User);
-      expect(foundUser.job).toBeDefined();
-      expect(foundUser.car).toBeDefined();
-      expect(foundUser.languages).toHaveLength(2);
-      expect(foundUser.languages).toIncludeAllMembers(['english', 'typescript']);
-      expect(foundUser.job.title).toEqual('Developer');
-      expect(foundUser.job.position).toEqual('Lead');
-      expect(foundUser.job.startedAt).toBeNumber();
-      expect(foundUser.job.jobType).not.toHaveProperty('_id');
-      expect(foundUser.job.jobType.salary).toEqual(5000);
-      expect(foundUser.job.jobType.field).toEqual('IT');
-      expect(foundUser.job.jobType.salary).toBeNumber();
-      expect((foundUser.car as CarType).model).toEqual('Tesla');
-      expect(foundUser.previousJobs).toHaveLength(2);
+    expect(foundUser.nick).toEqual('Nothing');
+    expect(foundUser.firstName).toEqual( 'John');
+    expect(foundUser.lastName).toEqual('Doe');
+    expect(foundUser.uniqueId).toEqual('john-doe-20');
+    expect(foundUser.age).toEqual(20);
+    expect(foundUser.gender).toEqual(Genders.MALE);
+    expect(foundUser.role).toEqual(Role.User);
+    expect(foundUser.job).toBeDefined();
+    expect(foundUser.car).toBeDefined();
+    expect(foundUser.languages).toHaveLength(2);
+    expect(foundUser.languages).toIncludeAllMembers(['english', 'typescript']);
+    expect(foundUser.job.title).toEqual('Developer');
+    expect(foundUser.job.position).toEqual('Lead');
+    expect(foundUser.job.startedAt).toBeNumber();
+    expect(foundUser.job.jobType).not.toHaveProperty('_id');
+    expect(foundUser.job.jobType.salary).toEqual(5000);
+    expect(foundUser.job.jobType.field).toEqual('IT');
+    expect(foundUser.job.jobType.salary).toBeNumber();
+    expect((foundUser.car as CarType).model).toEqual('Tesla');
+    expect(foundUser.previousJobs).toHaveLength(2);
 
-      expect(foundUser.fullName).toEqual('John Doe');
+    expect(foundUser.fullName).toEqual('John Doe');
 
-      const [janitor, manager] = _.sortBy(foundUser.previousJobs, ((job) => job.title));
-      expect(janitor.title).toEqual('Janitor');
-      expect(manager.title).toEqual('Manager');
+    const [janitor, manager] = _.sortBy(foundUser.previousJobs, ((job) => job.title));
+    expect(janitor.title).toEqual('Janitor');
+    expect(manager.title).toEqual('Manager');
 
-      expect(foundUser.previousCars).toHaveLength(2);
-      const [foundTrabant, foundZastava] =
+    expect(foundUser.previousCars).toHaveLength(2);
+    const [foundTrabant, foundZastava] =
         _.sortBy(foundUser.previousCars, (previousCar) => (previousCar as CarType).model);
-      expect((foundTrabant as CarType).model).toEqual('Trabant');
-      expect((foundTrabant as CarType).isSedan).toEqual(true);
-      expect((foundZastava as CarType).model).toEqual('Zastava');
-      expect((foundZastava as CarType).isSedan).toBeUndefined();
+    expect((foundTrabant as CarType).model).toEqual('Trabant');
+    expect((foundTrabant as CarType).isSedan).toEqual(true);
+    expect((foundZastava as CarType).model).toEqual('Zastava');
+    expect((foundZastava as CarType).isSedan).toBeUndefined();
+  });
 
-      foundUser.fullName = 'Sherlock Holmes';
-      expect(foundUser.firstName).toEqual('Sherlock');
-      expect(foundUser.lastName).toEqual('Holmes');
-
-      await foundUser.incrementAge();
-      expect(foundUser.age).toEqual(21);
-    }
-
-    {
-      const foundUser = await User.findByAge(21);
-      expect(foundUser.firstName).toEqual('Sherlock');
-      expect(foundUser.lastName).toEqual('Holmes');
-    }
-
-    {
-      const createdUser = await User.findOrCreate({
+  it ('should set the first name and last name', async () => {
+      const user = await User.create({
         firstName: 'Jane',
         lastName: 'Doe',
         gender: Genders.FEMALE,
+        age: 10,
       });
+      user.fullName = 'Sherlock Holmes';
+      expect(user.firstName).toEqual('Sherlock');
+      expect(user.lastName).toEqual('Holmes');
+  });
 
-      expect(createdUser).toBeTruthy();
-      expect(createdUser).toHaveProperty('created');
-      expect(createdUser.created).toEqual(true);
-      expect(createdUser).toHaveProperty('doc');
-      expect(createdUser.doc.firstName).toEqual('Jane');
+  it ('should increment the age', async () => {
+    const user = await User.create({
+      firstName: 'Jane',
+      lastName: 'Doe',
+      gender: Genders.FEMALE,
+      age: 10,
+    });
+    await user.incrementAge();
+    expect(user.age).toEqual(11);
+  });
 
-      const foundUser = await User.findOrCreate({
-        firstName: 'Jane',
-        lastName: 'Doe',
+  it('should fail to save because of duplicate unique key', async () => {
+    let err;
+    const uniqueId = 'test';
+
+    await User.ensureIndexes();
+
+    try {
+      await User.create({
+        _id: mongoose.Types.ObjectId(),
+        firstName: 'Bill',
+        lastName: 'Z',
+        gender: Genders.MALE,
+        uniqueId,
       });
-
-      expect(foundUser).toBeTruthy();
-      expect(foundUser).toHaveProperty('created');
-      expect(foundUser.created).toEqual(false);
-      expect(foundUser).toHaveProperty('doc');
-      expect(foundUser.doc.firstName).toEqual('Jane');
-
-      let err;
-      try {
-        const cloneUser = await User.create({
-          _id: mongoose.Types.ObjectId(),
-          firstName: 'John',
-          lastName: 'Doe',
-          age: 20,
-          gender: Genders.MALE,
-          uniqueId: 'john-doe-20',
-        });
-      } catch (e) {
-        err = e;
-      }
-      expect(err.name).toEqual('MongoError');
-      expect(err.code).toEqual(11000);
+      await User.create({
+        _id: mongoose.Types.ObjectId(),
+        firstName: 'Bill',
+        lastName: 'Z',
+        gender: Genders.MALE,
+        uniqueId,
+      });
+    } catch (e) {
+      err = e;
     }
+
+    expect(err).toBeDefined();
+    expect(err.name).toEqual('MongoError');
+    expect(err.code).toEqual(11000);
   });
 
   it('should add a language and job using instance methods', async () => {
